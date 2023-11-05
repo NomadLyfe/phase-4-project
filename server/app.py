75fb19bfc37288e098391cc6bdf4b7e4d6f83940
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-11-05 12:35:00 -0500

    created review and restaurant models, created password auth

--- server/app.py
+++ server/app.py
@@ -3,14 +3,14 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session, make_response, jsonify
 from flask_restful import Resource
 from flask.ext.bcrypt import Bcrypt
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-
+from models import User, Review
 
 bcrypt = Bcrypt(app)
 
@@ -19,6 +19,16 @@ bcrypt = Bcrypt(app)
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+@app.route('/login')
+def login():
+    username = request.get_jason()['username']
+    user = User.query.filter_by(username = username)
+    password = request.get_json()['password']
+    if user.authenticate(password):
+        session['user_id'] = user.id
+        return user.to_dict(), 200
+    return {'error': 'Invalid username or password'}, 401
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -5,7 +5,6 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from config import db
 from app import bcrypt
 
-# Models go here!
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
@@ -30,3 +29,20 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
     
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    #Realtionships here!
+    
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    address = db.Column(db.String)
+
+    #Realtionships here!
+
