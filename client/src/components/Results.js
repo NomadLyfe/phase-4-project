e60d6cd2c29b6495c6155fe7c19deab32ef42a54
Jeremy Garcia <jeremygarcia9400@gmail.com>
2024-01-12 16:37:17 -0500

    erased seed data, fixed rednering to display even with no seed data

--- client/src/components/Results.js
+++ client/src/components/Results.js
@@ -34,42 +34,43 @@ function Results({ results, history, onSearch, user }) {
     let renderedResultList = null
     if (results) {
         renderedResultList = results.map((result, index) => {
+            let matchedRestaurant = null
             if (restaurants) {
-                const matchedRestaurant = restaurants.find((restaurant) => {
+                matchedRestaurant = restaurants.find((restaurant) => {
                     return restaurant.name === result.name && restaurant.address === result.location.display_address[0]
                 })
-                let averageStars = null
-                let reviews = null
-                if (matchedRestaurant && matchedRestaurant.reviews) {
-                    const totalStars = matchedRestaurant.reviews.reduce((total, review) => total + review.stars, 0)
-                    reviews = matchedRestaurant.reviews.length
-                    if (reviews) {
-                        averageStars = Math.round((totalStars/reviews * 10))/10
-                    } else {
-                        averageStars = 0
-                    }
+            } else {
+                matchedRestaurant = null
+            }
+            let averageStars = null
+            let reviews = null
+            if (matchedRestaurant && matchedRestaurant.reviews) {
+                const totalStars = matchedRestaurant.reviews.reduce((total, review) => total + review.stars, 0)
+                reviews = matchedRestaurant.reviews.length
+                if (reviews) {
+                    averageStars = Math.round((totalStars/reviews * 10))/10
+                } else {
+                    averageStars = 0
                 }
-                return (
-                    <div className="result" key={result.id}>
-                        <img className="resultPic" alt="Restaurant" src={result.image_url} />
-                        <div className="resultInfo">
-                            <h2>{index + 1 + page}. <span>{result.name}</span></h2>
-                            <h3 onClick={onRestaurantClick} className="restuarantReviews"><span className="star">{'\u2605'.repeat(matchedRestaurant ? Math.round(averageStars) : 0)}</span>{'\u2606'.repeat(matchedRestaurant ? 5 - Math.round(averageStars) : 5)} {matchedRestaurant ? averageStars : 0} stars ( {matchedRestaurant ? reviews : 0} Reviews )</h3>
-                            <h4>Categories: {result.categories.map((row) => <span key={row.alias}>{row.title} </span>)}</h4>
-                            <h4>Price: {result.price ? result.price : '$'}</h4>
-                            <h4>{result.display_phone ? result.display_phone : '-no phone number available-'}</h4>
-                            <div className="address" >{result.location.display_address.map((row) => <h4 key={row}>{row}</h4>)}</div>
-                            <h4>{result.transactions.map((row, i) => <span key={i}>-{row === 'restaurant_reservation' ? 'reservation' : row}  </span>)}</h4>
-                            <div className="resultButtons">
-                                <button onClick={onRestaurantClick} className="resultButton">Reviews</button>
-                                <button onClick={onNewReviewClick} className="resultButton" >Leave a review</button>
-                            </div>
+            }
+            return (
+                <div className="result" key={result.id}>
+                    <img className="resultPic" alt="Restaurant" src={result.image_url} />
+                    <div className="resultInfo">
+                        <h2>{index + 1 + page}. <span>{result.name}</span></h2>
+                        <h3 onClick={onRestaurantClick} className="restuarantReviews"><span className="star">{'\u2605'.repeat(matchedRestaurant ? Math.round(averageStars) : 0)}</span>{'\u2606'.repeat(matchedRestaurant ? 5 - Math.round(averageStars) : 5)} {matchedRestaurant ? averageStars : 0} stars ( {matchedRestaurant ? reviews : 0} Reviews )</h3>
+                        <h4>Categories: {result.categories.map((row) => <span key={row.alias}>{row.title} </span>)}</h4>
+                        <h4>Price: {result.price ? result.price : '$'}</h4>
+                        <h4>{result.display_phone ? result.display_phone : '-no phone number available-'}</h4>
+                        <div className="address" >{result.location.display_address.map((row) => <h4 key={row}>{row}</h4>)}</div>
+                        <h4>{result.transactions.map((row, i) => <span key={i}>-{row === 'restaurant_reservation' ? 'reservation' : row}  </span>)}</h4>
+                        <div className="resultButtons">
+                            <button onClick={onRestaurantClick} className="resultButton">Reviews</button>
+                            <button onClick={onNewReviewClick} className="resultButton" >Leave a review</button>
                         </div>
                     </div>
-                )
-            } else {
-                return null
-            }
+                </div>
+            )
         })
     }
 
